fc4b3b91876f2d3ac6e23007346d117ec1ed3d12
thecommoncoder7 <65053816+thecommoncoder765@users.noreply.github.com>
2022-10-25 17:48:26 +0000

    Added extra commands  and changed bot grammar errors

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
 
+# when bot joins server
 @bot.event
 async def on_connect():
-  print('Hello! Your bot has went online!')
+  print('Hello! Your bot is now online!')
 
+#commands
 @bot.command
 async def ping(ctx):
   await ctx.send('pong!')
+  await ctx.send('Welcome to this server')
+  
+async def commands(ctx):
+  await ctx.send('Here are your commands that you can use:')
 
 token = os.environ['token']
 bot.run(token)
