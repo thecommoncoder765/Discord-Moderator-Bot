c95013ebcd05949087e6895dc6dc401f12782d51
thecommoncoder7 <65053816+thecommoncoder765@users.noreply.github.com>
2022-10-25 21:49:55 +0000

    Added some commands

--- main.py
+++ main.py
@@ -19,5 +19,13 @@ async def ping(ctx):
 async def commands(ctx):
   await ctx.send('Here are your commands that you can use:')
 
+async def help(ctx, arg):
+  if arg == "me":
+    await ctx.send('Nothing can fix you')
+  if arg != "me":
+    await ctx.send("I don't have a response for that")
+  if arg == "":
+    await ctx.send('I do not know what you need help with. Please specify with a space after "Help".')
+    
 token = os.environ['token']
 bot.run(token)
